f401aa3940be47ef97b856f56194dad986060fae
ilathem <isaiahlathem@gmail.com>
2023-11-13 20:47:31 -0600

    Add keymap to insert blank line below

--- lua/ilathem/remap.lua
+++ lua/ilathem/remap.lua
@@ -1,3 +1,7 @@
+-- ctrl+enter in insert mode continues onto a blank line (instead of inserting
+-- comment at the beginning if coming from a comment line)
+vim.keymap.set("i", "<C-CR>", "<Esc>o<Esc>0Di")
+
 -- change leader key to space (leader key is to enable another layer of keybinds)
 vim.g.mapleader = " "
 
