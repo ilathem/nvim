20dff9feb8dd745195ede8ffe16c3b9dc0de13e0
ilathem <isaiahlathem@gmail.com>
2024-03-18 21:43:04 -0500

    Change keybinds for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,10 +8,10 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 
 -- basic functions for navigating to various harpoon marks
-vim.keymap.set("n", "<D-j>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<D-k>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<D-l>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<D-;>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>;", function() ui.nav_file(4) end)
 
 
 
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,13 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 -- space+p+f opens telescope (pf stands for project files)
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- Ctrl+p is for git files in particular (is faster) 
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+-- Ctrl+p is for git files in particular (is faster)
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 -- space+p+s for project search
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+
+-- these are the default keybinds
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
