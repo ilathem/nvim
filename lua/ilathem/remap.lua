fe509c5d0b124add1f02783e9f3bc355ecdde9e7
ilathem <isaiahlathem@gmail.com>
2024-03-22 17:08:41 -0500

    Add ampersand

--- lua/ilathem/remap.lua
+++ lua/ilathem/remap.lua
@@ -19,7 +19,8 @@ vim.keymap.set("n", "<leader>gg", function()
         vim.cmd("silent !git add .")
         vim.cmd(command)
         vim.cmd("silent !git push --quiet")
-        vim.cmd("!echo Success! ${input}");
+        -- vim.cmd("echo Success! ${input}");
+        vim.cmd("echo Success! &input");
     end)
 end)
 
