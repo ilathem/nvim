a612c1be8ad064626c13f47de7dd435ce6345319
ilathem <isaiahlathem@gmail.com>
2024-03-22 17:15:34 -0500

    Move quotes around

--- lua/ilathem/remap.lua
+++ lua/ilathem/remap.lua
@@ -20,7 +20,7 @@ vim.keymap.set("n", "<leader>gg", function()
         vim.cmd(command)
         vim.cmd("silent !git push --quiet")
         -- vim.cmd("echo Success! ${input}");
-        local echoString = "Success! " .. input;
+        local echoString = "Success! '".. input .."' ";
         vim.cmd.echo(echoString);
     end)
 end)
