c1f0b7d77f67851a3117d5c8252f2f300b479840
ilathem <isaiahlathem@gmail.com>
2024-03-30 11:07:18 -0500

    Auto format css on filesave using prettier

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -5,6 +5,7 @@ require("conform").setup({
     python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
+    css = { { "prettierd", "prettier" } }
   },
 })
 
