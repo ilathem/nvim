ca1b90a4fce2d0a0cf0c42153b923e6658a98ce2
ilathem <isaiahlathem@gmail.com>
2024-05-05 08:49:11 -0500

    Change harpoon keys to work on laptop

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,10 +8,10 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 
 -- basic functions for navigating to various harpoon marks
-vim.keymap.set("n", "<M-j>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<M-k>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<M-l>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<M-;>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<D-j>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<D-k>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<D-l>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<D-;>", function() ui.nav_file(4) end)
 
 
 
