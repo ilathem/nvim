683f1e881f0f14cfd66dcf4d7e2192527d993fa9
Isaiah Lathem <isaiahlathem@gmail.com>
2024-06-19 22:37:08 -0500

    Don't push with gg command

--- lua/ilathem/remap.lua
+++ lua/ilathem/remap.lua
@@ -21,8 +21,7 @@ vim.keymap.set("n", "<leader>gg", function()
         local command = "silent !git commit -m '" .. input .. "' --quiet";
         vim.cmd("silent !git add .")
         vim.cmd(command)
-        vim.cmd("silent !git push --quiet")
-        vim.cmd("echo 'Success! => " .. input .. "'");
+        vim.cmd("echo 'Commit made with message! => " .. input .. "'");
     end)
 end)
 
